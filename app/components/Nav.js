45f5fd40f158e2ab088510549639ced616d9e825
yabets <yabetsbelay@gmail.com>
2021-01-10 18:21:45 +0300

    final commit

---
+++ app/components/Nav.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {ThemeConsumer} from "../context/theme";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = {
+  color: "rgb(187,46, 31)"
+}
+
+function Nav() {
+  return (
+    <ThemeConsumer>
+      {({theme, toggleTheme}) => (
+        <nav className="row space-between">
+          <ul className="row nav">
+            <li>
+              <NavLink 
+                to="/" 
+                exact
+                activeStyle={activeStyle}
+                className="nav-link">
+                  Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/battle" 
+                activeStyle={activeStyle}
+                className="nav-link">
+                  Battle
+                </NavLink>
+              </li>
+          </ul>
+          <button 
+            style={{fontSize: 30}}
+            className='btn-clear'
+            onClick={toggleTheme}>
+              {theme === 'light' ? '🔦️' : '💡️'}
+            </button>
+        </nav>
+      )}
+    </ThemeConsumer>
+  )
+}
+
+export default Nav;
---
+++ app/utils/api.js
@@ -0,0 +1,69 @@
+
+function getErrorMsg(message, username) {
+    if(message === 'Not Found') {
+        return `${username} doesn't exist`;
+    }
+    return message;
+}
+function getProfile(username) {
+    return fetch(`https://api.github.com/users/${username}`)
+        .then((res) => res.json())
+        .then((profile) => {
+            console.log(profile);
+            if(profile.message) {
+                throw new Error(getErrorMsg(profile.message, username))
+            }
+            return profile;
+        })
+}
+
+function getRepos(username) {
+    return fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
+        .then((res) => res.json())
+        .then((repos) => {
+            if(repos.message) {
+                throw new Error(getErrorMsg(repos.message, username))
+            }
+            return repos;
+        })
+}
+
+function getStarCount(repos) {
+    return repos.reduce((count, {stargazers_count }) => count + stargazers_count, 0)
+}
+function calculateScore(followers, repos) {
+    return (followers * 3 ) + getStarCount(repos);
+}
+
+function getUserDate(player) {
+    return Promise.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then(([profile, repos]) => ({
+        profile,
+        score: calculateScore(profile.followers, repos)
+    }))
+}
+
+function sortPlayers(players) {
+    return players.sort((a, b) => b.score - a.score)
+}
+export function battle(players) {
+    return Promise.all([
+        getUserDate(players[0]),
+        getUserDate(players[1])
+    ]).then((res) => sortPlayers(res))
+}
+
+export function fetchPopularRepos(language) {
+    const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+
+    return fetch(endpoint)
+        .then((res) => res.json())
+        .then((data) => {
+            if(!data.items) {
+                throw new Error(data.message);
+			}
+			return data.items;
+        })
+}
